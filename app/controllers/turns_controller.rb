e615bb77583e8c2ab0aceaeec48b94233c1c4a8c
Vishal Arora <vishal@vishalarora.com>
2015-10-30 01:07:04 -0400

    Skip CSRF check

--- app/controllers/turns_controller.rb
+++ app/controllers/turns_controller.rb
@@ -1,5 +1,6 @@
 class TurnsController < ApplicationController
   before_action :set_turn, only: [:show, :edit, :update, :destroy]
+  skip_before_filter  :verify_authenticity_token
 
   # GET /turns
   # GET /turns.json
